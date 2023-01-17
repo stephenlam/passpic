ad6032df9f3d4cd055cb248207ab53c908957090
Stephen Lam <stephenlam@users.noreply.github.com>
2023-01-17 16:51:43 -0500

    Annotations in text file instead of command line.

--- annotation.py
+++ annotation.py
@@ -1,15 +1,28 @@
+from pathlib import Path
+
 class Annotation:
     # Coordinate system has (0, 0) at top-left corner of image.
-    def __init__(self, filename, x, y, w, h, eye_y):
+    def __init__(self, filename, x1, y1, x2, y2, eye_y):
         self.filename = filename
-        self.x = x  # x-coord of top corner of head
-        self.y = y  # y-coord of top corner of head
-        self.w = w  # width of head in pixels
-        self.h = h  # height of head in pixels
+        self.x1 = x1  # x-coord of top-left corner of head
+        self.y1 = y1  # y-coord of top-left corner of head
+        self.x2 = x2  # x-coord of bottom-right corner of head
+        self.y2 = y2  # y-coord of bottom-right corner of head
         self.eye_y = eye_y  # y-coord of eye center (eye level)
+        self.x = min(x1, x2)
+        self.y = min(y1, y2)
+        self.w = abs(x2 - x1)
+        self.h = abs(y2 - y1)
 
     def __repr__(self):
         return f"Annotation(filename={self.filename}, " \
-               f"x={self.x}, y={self.y}, " \
-               f"w={self.w}, h={self.h}, " \
-               f"eye_y={self.eye_y}"
+               f"x1={self.x1}, y1={self.y1}, " \
+               f"x2={self.x2}, y2={self.y2}, " \
+               f"eye_y={self.eye_y})"
+
+def load(filename):
+    path = Path(filename)
+    anno_path = path.with_suffix(".txt")
+    with open(anno_path, 'r') as f:
+        values = [int(v) for v in f.read().strip().split()]
+        return Annotation(path, *values)
--- passpic.py
+++ passpic.py
@@ -5,56 +5,33 @@ import subprocess
 import annotation
 import passport_usa
 
-def get_annotation():
-    infile = Path(sys.argv[1])
-    x1 = int(sys.argv[2])
-    y1 = int(sys.argv[3])
-    x2 = int(sys.argv[4])
-    y2 = int(sys.argv[5])
-    eye_y = int(sys.argv[6])
-
-    x = min(x1, x2)
-    y = min(y1, y2)
-    w = abs(x2 - x1)
-    h = abs(y2 - y1)
-
-    ann = annotation.Annotation(infile, x, y, w, h, eye_y)
-    return ann
-
-
 def main():
-    if len(sys.argv) < 7:
+    if len(sys.argv) < 2:
         print("""Usage:
-    python3 passpic.py infile x1 y1 x2 y2 eye_y
+    python3 passpic.py infile
 
 Arguments:
     infile: path to image to be processed
-    x1: x-coordinate of top-left corner of head
-    y1: y-coordinate of top-left corner of head
-    x2: x-coordinate of bottom-right corner of head
-    y2: y-coordinate of bottom-right corner of head
-    eye_y: y-coordinate of eye center (eye level)
 """)
         sys.exit(1)
 
     outdir = Path("out")
     outdir.mkdir(parents=True, exist_ok=True)
 
-    ann = get_annotation()
+    ann = annotation.load(sys.argv[1])
 
     stem = ann.filename.stem
-    outfile = outdir / f"passport4x6_{stem}_extent.png"
-    cmdlog = outdir / f"passport4x6_{stem}_extent.log"
+    outfile = outdir / f"passport4x6_{stem}.png"
+    cmdlog = outdir / f"passport4x6_{stem}.log"
     cmd = passport_usa.create_passport_4_6_cmd(ann, outfile)
     str_cmd = ' '.join([str(t) for t in cmd])
 
+    print(f"Running command: \n{str_cmd}")
+    subprocess.run(cmd)
+
     with open(cmdlog, "w", encoding="utf-8") as f:
-        f.write(f"python3 {' '.join(sys.argv)}\n")
         f.write(f"{ann}\n")
         f.write(f"{str_cmd}\n")
-
-    print(f"Running imagemagick command: \n{str_cmd}")
-    subprocess.run(cmd)
 
 
 if __name__ == "__main__":
