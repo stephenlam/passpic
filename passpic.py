75bb7e497c47ee49766f7f8772106abf9c1cfc27
Stephen Lam <stephenlam@users.noreply.github.com>
2023-01-18 09:56:44 -0500

    Add annotation examples to usage string.

--- passpic.py
+++ passpic.py
@@ -13,7 +13,23 @@ def main():
 
 Arguments:
     infile: path to image to be processed
-""")
+
+For each image, passpic requires a corresponding annotation file
+with these five parameters:
+    x1: x-coordinate of top-left corner of head
+    y1: y-coordinate of top-left corner of head
+    x2: x-coordinate of bottom-right corner of head
+    y2: y-coordinate of bottom-right corner of head
+    eye_y: y-coordinate of eye center (eye level)
+
+The parameters should be integers separated by single spaces on a single line.
+
+Example:
+1024 1260 1424 1860 1585
+
+The annotation file should be located in the same directory as the image file,
+with a suffix .txt. For instance, the annotation file for john.jpg should be
+john.txt.""")
         sys.exit(1)
 
     outdir = Path("out")
@@ -21,6 +37,7 @@ Arguments:
 
     ann = annotation.load(sys.argv[1])
 
+    print("Creating US passport photo.")
     stem = ann.filename.stem
     outfile = outdir / f"passport4x6_{stem}.png"
     cmdlog = outdir / f"passport4x6_{stem}.log"
@@ -30,6 +47,8 @@ Arguments:
     print(f"Running command: \n{str_cmd}")
     subprocess.run(cmd)
 
+    print()
+    print("Creating Chinese visa photo.")
     with open(cmdlog, "w", encoding="utf-8") as f:
         f.write(f"{ann}\n")
         f.write(f"{str_cmd}\n")
